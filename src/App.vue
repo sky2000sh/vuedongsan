d17522b1230165ab21aa1113e920058bd7a09918
sky2000sh <sky2000sh@gmail.com>
2022-05-26 00:00:39 +0900

    second commit on 25/05/22

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="menu">
+    <!-- 반복적인 같은 태그를 효율적으로 돌리기 위해서는 v-for 반복문
+    1. 원하는 태그에 v-for="작명 in 반복할횟수"
+    2. 그리고 반복문에는 반드시 센스있게 :key="작명" 이것도 추가
+    그럼 이 HTML 태그는 원하는 만큼 반복생성
+    작명한 변수는 데이터안의 '자료'가 된다. -->
+    <a v-for="menu in menuBar" :key="menu">{{ menu }}</a>
+  </div>
   <div>
     원룸샵
     <h4 class="red" :style="스타일">{{ products[0] }}</h4>
@@ -13,6 +20,11 @@
   <div>
     <h4>{{ products[2] }}</h4>
     <p>{{ price2 }} 만원</p>
+  </div>
+  <hr>
+  <div>
+    <h4 v-for="product in products" :key="product">{{ product }}</h4>
+    <p v-for="price in prices" :key="price">{{price}}만원</p>
   </div>
 
 </template>
@@ -35,7 +47,9 @@ export default {
       //price1: 60,
       //price2: 70,
       //스타일: 'color : blue',
+      menuBar : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      prices : ['50', '60', '70'],
 
     }
 
@@ -50,6 +64,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
