3015b410251dd293c6ba93ef6d8478e472c45c7b
sky2000sh <sky2000sh@gmail.com>
2022-06-11 21:49:22 +0900

    input on 11/06/22

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -4,8 +4,18 @@
       <img :src="roomData[pressId].image" style="width: 100%"/>
       <h4>{{ roomData[pressId].title }}</h4>
       <p>{{ roomData[pressId].content }}</p>
-      <p>{{ roomData[pressId].price }}원</p>
-      <!-- <button @click="openModal = false">닫기</button> -->
+      <!-- <input @input="month = $event.target.value"> -->
+      <!-- $event.target.value => 사용자가 input 태그에 입력한 글자값 -->
+      <input v-model.number="month">
+      <!-- <textarea v-model="month"></textarea> -->
+      <!-- <select v-model="month">
+        <option>123</option>
+        <option>456</option>
+        <option>789</option>
+      </select> -->
+      <input type="checkbox" v-model="month">
+      <p> {{ month }}개월 : {{ roomData[pressId].price * month}}원</p>
+      <!-- <button @click="openModal = false">닫기</button> -->      
       <button @click="$emit('openModal')">닫기</button>
     </div>
   </div>
@@ -24,7 +34,7 @@ export default {
 
     data() {
         return {
-            
+            month: 1,
         }
     }
 
